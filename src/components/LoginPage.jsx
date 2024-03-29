edc2c48d084d309c1c38d38fd09f9f82e25966da
leesangdal <asksa1256@gmail.com>
2024-03-29 20:42:45 +0900

    로그인 화면 추가중

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./LoginPage.scss";
+
+function LoginPage() {
+  return (
+    <div className="LoginPage">
+      <form>
+        <figure className="logo">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/logo.svg`}
+            alt="구독의 민족"
+          />
+        </figure>
+        <h4>로그인</h4>
+        <input type="text" className="form-control" />
+      </form>
+    </div>
+  );
+}
+export default LoginPage;
